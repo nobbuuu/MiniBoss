66b5dfb92baecc9e35c48cccdea8de4d58d41331
heguicun <heguicun7878@foxmail.com>
2022-06-28 14:22:08 +0800

    [黑名单]

--- app/src/main/java/com/dream/miniboss/mine/ui/FeedbackActivity.java
+++ app/src/main/java/com/dream/miniboss/mine/ui/FeedbackActivity.java
@@ -3,12 +3,14 @@ package com.dream.miniboss.mine.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.dream.miniboss.R;
 import com.dream.miniboss.base.BaseActivity;
+import com.hjq.bar.TitleBar;
 
 public class FeedbackActivity extends BaseActivity {
-
+    TitleBar mTitleBar;
 
     @Override
     protected int initLayout() {
@@ -17,11 +19,21 @@ public class FeedbackActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        mTitleBar = fvbi(R.id.title_feed_back);
     }
 
     @Override
     protected void initData() {
+        event();
+    }
 
+    private void event() {
+        mTitleBar.getLeftView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onBackPressed();
+            }
+        });
     }
 }
