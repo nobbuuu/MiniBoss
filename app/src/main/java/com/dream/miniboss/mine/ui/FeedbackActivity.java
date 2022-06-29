62c910d3aa4a31be56bde613a83aded89183e3e1
heguicun <heguicun7878@foxmail.com>
2022-06-30 00:13:56 +0800

    [黑名单]

--- app/src/main/java/com/dream/miniboss/mine/ui/FeedbackActivity.java
+++ app/src/main/java/com/dream/miniboss/mine/ui/FeedbackActivity.java
@@ -2,16 +2,21 @@ package com.dream.miniboss.mine.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.dream.miniboss.R;
 import com.dream.miniboss.base.BaseActivity;
 import com.hjq.bar.TitleBar;
+import com.ruffian.library.widget.RTextView;
 
 public class FeedbackActivity extends BaseActivity {
     TitleBar mTitleBar;
-
+    RTextView mTextViewSoftNoUse;
+    boolean flag=false;
     @Override
     protected int initLayout() {
         return R.layout.activity_feedback;
@@ -20,6 +25,7 @@ public class FeedbackActivity extends BaseActivity {
     @Override
     protected void initView() {
         mTitleBar = fvbi(R.id.title_feed_back);
+        mTextViewSoftNoUse=fvbi(R.id.rtv_soft_notUse);
     }
 
     @Override
@@ -28,6 +34,7 @@ public class FeedbackActivity extends BaseActivity {
     }
 
     private void event() {
+        //点击标题栏的返回按钮
         mTitleBar.getLeftView().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,5 +42,22 @@ public class FeedbackActivity extends BaseActivity {
                 onBackPressed();
             }
         });
+        //点击RTextview
+     mTextViewSoftNoUse.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if (flag==false){
+                Log.i(TAG, "onClick: "+mTextViewSoftNoUse.getText());
+               //mTextViewSoftNoUse.getHelper().getIconPressedRight().setVisible(false,false);
+                mTextViewSoftNoUse.getHelper().getIconPressedRight().setVisible(true,false);
+                flag=true;
+            }else {
+                Log.i(TAG, "onClick: "+mTextViewSoftNoUse.getHelper().getIconPressedRight());
+                mTextViewSoftNoUse.getHelper().getIconPressedRight().setVisible(false,false);
+                flag=false;
+            }
+         }
+     });
+
     }
 }
--- app/src/main/java/com/dream/miniboss/mine/ui/PermissionSettingActivity.java
+++ app/src/main/java/com/dream/miniboss/mine/ui/PermissionSettingActivity.java
@@ -1,6 +1,8 @@
 package com.dream.miniboss.mine.ui;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -28,6 +30,9 @@ public class PermissionSettingActivity extends BaseActivity implements CompoundB
     TitleBar mTitleBar;
     Switch localSwitch, calendarSwitch, cameraSwitch, voiceSwitch, deviceSwitch, albumSwitch;
     String[] permissionsLocal,permissionsCamera,permissionsVoice,permissionsCalendar,permissionsDevice;
+    //设置一个存储状态
+    final boolean falg = true;
+    SharedPreferences preferences;
     @Override
     protected int initLayout() {
         return R.layout.activity_setting_permission;
@@ -46,12 +51,47 @@ public class PermissionSettingActivity extends BaseActivity implements CompoundB
         permissionsLocal = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION
         };
         permissionsCamera=new String[]{Manifest.permission.CAMERA};
-        permissionsVoice=new String[]{ Manifest.permission.ADD_VOICEMAIL};
+        permissionsVoice=new String[]{ Manifest.permission.ADD_VOICEMAIL,Manifest.permission.RECORD_AUDIO};
         permissionsCalendar=new String[]{Manifest.permission.WRITE_CALENDAR,Manifest.permission.READ_CALENDAR};
         permissionsDevice=new String[]{Manifest.permission.READ_PHONE_STATE};
          PermissionsUtils.showSystemSetting = false;//是否支持显示系统设置权限设置窗口跳转
         //这里的this不是上下文，是Activity对象！
-
+        // 从SharedPreferences获取数据:显示位置权限状态
+        preferences = getSharedPreferences("local", Context.MODE_PRIVATE);
+        if (preferences != null) {
+            boolean name = preferences.getBoolean("flag", falg);
+            localSwitch.setChecked(name);
+        }
+        //从SharedPreferences获取数据:显示日历权限状态
+        preferences = getSharedPreferences("calendar", Context.MODE_PRIVATE);
+        if (preferences != null) {
+            boolean name = preferences.getBoolean("flag", falg);
+            calendarSwitch.setChecked(name);
+        }
+        //从SharedPreferences获取数据:显示相机权限状态
+        preferences = getSharedPreferences("camera", Context.MODE_PRIVATE);
+        if (preferences != null) {
+            boolean name = preferences.getBoolean("flag", falg);
+            cameraSwitch.setChecked(name);
+        }
+        //从SharedPreferences获取数据:显示麦克风权限状态
+        preferences = getSharedPreferences("voice", Context.MODE_PRIVATE);
+        if (preferences != null) {
+            boolean name = preferences.getBoolean("flag", falg);
+            voiceSwitch.setChecked(name);
+        }
+        //从SharedPreferences获取数据:显示设备信息权限状态
+        preferences = getSharedPreferences("device", Context.MODE_PRIVATE);
+        if (preferences != null) {
+            boolean name = preferences.getBoolean("flag", falg);
+            deviceSwitch.setChecked(name);
+        }
+        //从SharedPreferences获取数据:显示相册权限状态
+        preferences = getSharedPreferences("album", Context.MODE_PRIVATE);
+        if (preferences != null) {
+            boolean name = preferences.getBoolean("flag", falg);
+            albumSwitch.setChecked(name);
+        }
     }
 
     @Override
@@ -97,62 +137,116 @@ public class PermissionSettingActivity extends BaseActivity implements CompoundB
         switch (buttonView.getId()){
             case R.id.switch_permission_local:
                 if (localSwitch.isChecked()) {
-                Log.d(TAG, "onCheckedChanged: " + "这是位置权限" +permissionsLocal);
                 PermissionsUtils.getInstance().chekPermissions(this, permissionsLocal, permissionsResult);
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("local", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", true);
+                    editor.commit();
                 }else {
                     Toast.makeText(PermissionSettingActivity.this, "请滑动按钮申请权限!", Toast.LENGTH_SHORT).show();
-
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("local", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", false);
+                    editor.commit();
                 }
                 break;
             case R.id.switch_permission_canlder:
                 if (calendarSwitch.isChecked()) {
                     Log.d(TAG, "onCheckedChanged: " + "日历权限" +permissionsCalendar);
                     PermissionsUtils.getInstance().chekPermissions(this, permissionsCalendar, permissionsResult);
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("calendar", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", true);
+                    editor.commit();
                 }else {
                     Toast.makeText(PermissionSettingActivity.this, "请滑动按钮申请权限!", Toast.LENGTH_SHORT).show();
-
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("calendar", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", false);
+                    editor.commit();
                 }
                 break;
             case R.id.switch_permission_camera:
                 if (cameraSwitch.isChecked()) {
                     Log.d(TAG, "onCheckedChanged: " + "日历权限" +permissionsCamera);
                     PermissionsUtils.getInstance().chekPermissions(this, permissionsCamera, permissionsResult);
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("camera", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", true);
+                    editor.commit();
                 }else {
                     Toast.makeText(PermissionSettingActivity.this, "请滑动按钮申请权限!", Toast.LENGTH_SHORT).show();
-
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("camera", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", false);
+                    editor.commit();
                 }
                 break;
             case R.id.switch_permission_voice:
                 if (voiceSwitch.isChecked()) {
                     Log.d(TAG, "onCheckedChanged: " + "日历权限" +permissionsVoice);
                     PermissionsUtils.getInstance().chekPermissions(this, permissionsVoice, permissionsResult);
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("voice", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", true);
+                    editor.commit();
                 }else {
                     Toast.makeText(PermissionSettingActivity.this, "请滑动按钮申请权限!", Toast.LENGTH_SHORT).show();
-
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("voice", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", false);
+                    editor.commit();
                 }
                 break;
             case R.id.switch_permission_device:
                 if (deviceSwitch.isChecked()) {
                     Log.d(TAG, "onCheckedChanged: " + "日历权限" +permissionsDevice);
                     PermissionsUtils.getInstance().chekPermissions(this, permissionsDevice, permissionsResult);
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("device", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", true);
+                    editor.commit();
                 }else {
                     Toast.makeText(PermissionSettingActivity.this, "请滑动按钮申请权限!", Toast.LENGTH_SHORT).show();
-
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("device", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", false);
+                    editor.commit();
                 }
                 break;
             case R.id.switch_permission_album:
                 if (albumSwitch.isChecked()) {
                     Log.d(TAG, "onCheckedChanged: " + "日历权限" +permissionsCamera);
                     PermissionsUtils.getInstance().chekPermissions(this, permissionsCamera, permissionsResult);
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("album", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", true);
+                    editor.commit();
                 }else {
                     Toast.makeText(PermissionSettingActivity.this, "请滑动按钮申请权限!", Toast.LENGTH_SHORT).show();
-
+                    //将数据保存至SharedPreferences:
+                    SharedPreferences preferences = getSharedPreferences("album", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("flag", false);
+                    editor.commit();
                 }
                 break;
             default:
         }
 
     }
+
     //创建监听权限的接口对象
     PermissionsUtils.IPermissionsResult permissionsResult = new PermissionsUtils.IPermissionsResult() {
         @Override
