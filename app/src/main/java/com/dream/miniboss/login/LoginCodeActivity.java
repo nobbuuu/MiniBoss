93b53b2b2049b010fc9bfd8f08bad08578f1e0e9
heguicun <heguicun7878@foxmail.com>
2022-07-02 11:45:10 +0800

    [登录相关]

---
+++ app/src/main/java/com/dream/miniboss/login/LoginCodeActivity.java
@@ -0,0 +1,27 @@
+package com.dream.miniboss.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dream.miniboss.R;
+import com.dream.miniboss.base.BaseActivity;
+
+public class LoginCodeActivity extends BaseActivity {
+
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_login_code;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/dream/miniboss/login/LoginPhoneActivity.java
@@ -0,0 +1,27 @@
+package com.dream.miniboss.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dream.miniboss.R;
+import com.dream.miniboss.base.BaseActivity;
+
+public class LoginPhoneActivity extends BaseActivity {
+
+
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_login_phone;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
