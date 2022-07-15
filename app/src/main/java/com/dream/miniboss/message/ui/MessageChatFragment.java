12d87f15733507f13df5d3448915d56cc753e883
Administrator <986812333@qq.com>
2022-07-15 20:52:04 +0800

    “更新”

--- app/src/main/java/com/dream/miniboss/message/ui/MessageChatFragment.java
+++ app/src/main/java/com/dream/miniboss/message/ui/MessageChatFragment.java
@@ -2,10 +2,14 @@ package com.dream.miniboss.message.ui;
 
 import android.view.View;
 
+import com.dream.miniboss.MiniBossAppKt;
 import com.dream.miniboss.R;
 import com.dream.miniboss.base.BaseFragment;
+import com.hjq.bar.TitleBar;
 
 public class MessageChatFragment  extends BaseFragment {
+    TitleBar mTitleBar;
+
     @Override
     protected int setLayout() {
         return R.layout.fragment_chat_message;
@@ -13,12 +17,21 @@ public class MessageChatFragment  extends BaseFragment {
 
     @Override
     protected void initView() {
-
+        mTitleBar=fvbyid(R.id.title_bar_message);
     }
 
     @Override
     protected void initData() {
+    event();
+    }
 
+    private void event() {
+        mTitleBar.getLeftView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
     }
 
     @Override
