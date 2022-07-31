58665f8e839998e42ab646f415b6f3a27652dc02
Administrator <986812333@qq.com>
2022-07-31 18:24:12 +0800

    "消息界面"

--- app/src/main/java/com/dream/miniboss/login/LoginCodeActivity.java
+++ app/src/main/java/com/dream/miniboss/login/LoginCodeActivity.java
@@ -2,45 +2,26 @@ package com.dream.miniboss.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import com.dream.miniboss.R;
 import com.dream.miniboss.base.BaseActivity;
+import com.hjq.bar.TitleBar;
 
 public class LoginCodeActivity extends BaseActivity {
-    Button getCodeButton;
-   ImageView mView;
-
+     TitleBar mTitle;
     @Override
     protected int initLayout() {
-        return R.layout.activity_login_code;
+        return R.layout.activity_login;
     }
 
     @Override
     protected void initView() {
-        getCodeButton=fvbi(R.id.btn_get_code);
-        mView=fvbi(R.id.iv_gouxuan);
+     mTitle=fvbi(R.id.title_login_code);
     }
 
     @Override
     protected void initData() {
-     getCodeButton.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             startActivity(new Intent(LoginCodeActivity.this,LoginCodeNumActivity.class));
-         }
-     });
-     //勾选按钮的选择
-        mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mView.setImageResource(R.mipmap.gouxuan);
-            }
-        });
-
+    mTitle.getLeftIcon().isVisible();
     }
 }
